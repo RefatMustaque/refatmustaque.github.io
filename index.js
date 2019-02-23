94b27bc10e6ffbb3f2848a2d6c7f3276ce034f5b
Refat Bin Mustaque <refatmustaque@gmail.com>
2019-02-23 22:56:25 +0600

    add load content script to external file

---
+++ index.js
@@ -0,0 +1,10 @@
+$(document).ready(function () {
+    $(".nav-tabs a").click(function () {
+        $(this).tab("show");
+    });
+
+    // navigation content
+    $("#home").load("Content/home.html");
+
+    $("#project").load("Content/project.html");
+});
