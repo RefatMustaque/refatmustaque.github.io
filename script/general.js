bcdcaeba94c94524f5b80aa4ab9c9caa9b100bdc
Refat Bin Mustaque <refatmustaque@gmail.com>
2020-11-22 19:21:59 +0600

    update all

--- script/general.js
+++ script/general.js
@@ -1,16 +1,16 @@
+loadTopBar();
+if(window.location.href.substring(window.location.href.lastIndexOf('/') + 1).split(".")[0])
+{
+  loadSideBar(window.location.href.substring(window.location.href.lastIndexOf('/') + 1).split(".")[0]);
+}
+else {
+  loadSideBar("index");
+}
+
+
+$("body").on("click", "#menu-toggle", function(e) {
+  toggleSidebarMenu(e);
+});
+
 // When the user scrolls down 20px from the top of the document, show the button
 window.onscroll = function() {scrollFunction()};
-
-function scrollFunction() {
-  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
-    $("#scrollToTopBtn").css("display", "block");
-  } else {
-    $("#scrollToTopBtn").css("display", "none");
-  }
-}
-
-// When the user clicks on the button, scroll to the top of the document
-function topFunction() {
-  document.body.scrollTop = 0; // For Safari
-  document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
-}
